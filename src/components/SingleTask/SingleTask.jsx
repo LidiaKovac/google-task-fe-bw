85b39d5ea234c63bbc965acfee1521649ffac2f9
Bogdan Birau <birau.bogdan92@gmail.com>
2021-12-21 11:25:45 +0100

    update

--- src/components/SingleTask/SingleTask.jsx
+++ src/components/SingleTask/SingleTask.jsx
@@ -1,38 +1,88 @@
-import "./SingleTask.css"
-import { BsCircle, BsCheckCircle } from "react-icons/bs"
-import { useEffect, useState } from "react"
+import "./SingleTask.css";
+import { BsCircle, BsCheckCircle } from "react-icons/bs";
+import { useEffect, useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+import "../Modal/Modal.css";
 
 export const SingleTask = ({ content, id, setDone }) => {
-  const [isChecked, setChecked] = useState(false)
-  const [singleTask, setSingleTask] = useState([])
+  const [isChecked, setChecked] = useState(false);
+  const [singleTask, setSingleTask] = useState([]);
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const fetchSingleTask = async () => {
     try {
       let response = await fetch(`%{process.env.REACT_APP_BE_PROD_URL}/tasks`);
       if (response.ok) {
-        let singleTaskData = await response.json()
-        setSingleTask(singleTaskData)   
+        let singleTaskData = await response.json();
+        setSingleTask(singleTaskData);
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
-  useEffect(()=> {
-    if(isChecked) { //if the task has been marked as done
-      setTimeout(()=> {
-        setDone(id)
-      }, 500)
+  useEffect(() => {
+    if (isChecked) {
+      //if the task has been marked as done
+      setTimeout(() => {
+        setDone(id);
+      }, 500);
       console.log("isChecked");
     }
-  }, [isChecked])
+  }, [isChecked]);
+
+  const updateTask = async () => {
+    try {
+      let response = await fetch(`https://google-task-backend-strive.herokuapp.com/tasks/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          content,
+        }),
+      });
+
+      if (response.ok) {
+        console.log("Task updated");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <div className="single__wrap">
-      <div className="single__checkmark" onClick={() => setChecked((check) => !check)}>
-        {!isChecked ? <BsCircle /> : <BsCheckCircle />}
+    <>
+      <div className="single__wrap">
+        <div className="single__checkmark" onClick={() => setChecked((check) => !check)}>
+          {!isChecked ? <BsCircle /> : <BsCheckCircle />}
+        </div>
+        <div className="single__content"> {content} </div>
       </div>
-      <div className="single__content"> {content} </div>
-    </div>
-  )
-}
+      <div>
+        <Button variant="info" onClick={handleShow}>
+          Update
+        </Button>
+        <Button variant="danger">Delete</Button>
+      </div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Body>
+          <h2>Update task</h2>
+          <small>Press enter to update</small>
+          <input type="text" onKeyUp={(e) => updateTask(e)} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={updateTask()}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
